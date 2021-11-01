f86f46bc43778f48d5b63db6c2ace0486c5a812a
esadh <esadh@ESAD>
2021-11-01 14:37:20 +0300

    Proje dosyası ekle.

---
+++ HashTable_uygulamasi/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+
+namespace HashTable_uygulamasi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //HASHTABLE uygulaması
+            Console.WriteLine("Başlık giriniz: ");
+            string Baslik = Console.ReadLine();
+
+            //küçültme işlemi
+            Baslik = Baslik.ToLower();
+            //Hastable 
+            var karakter_seti = new Hashtable()
+            {
+                {'ç','c' },
+                {'ı','i' },
+                {'ö','o' },
+                {'ü','u' },
+                {' ','-' },
+                {'ğ','g' },
+                {'\'','-' },
+            };
+
+            foreach (DictionaryEntry item in karakter_seti)
+            {
+                Baslik = Baslik.Replace((char)item.Key,(char)item.Value);
+            }
+
+            //yeni balığı ekrana yazdırma
+            Console.WriteLine(Baslik);
+
+            Console.ReadKey();
+        }
+    }
+}
